f72ca8a96dc4e0b6275284c8b36ed413c2111b57
necklace <xiangxn@163.com>
2021-12-17 02:31:15 +0800

    init

--- index.test.js
+++ index.test.js
@@ -5,7 +5,7 @@ const path = require('path');
 test('test runs', () => {
     process.env['INPUT_TEMPLATE'] = '';
     process.env['INPUT_CATEGORIES'] = '[{"title":"New features","labels":["feature","enhancement"]},{"title":"Bug fixes and improvements","labels":["bug"]}]';
-    const ip = path.join(__dirname, 'index.js');
-    const result = cp.execSync(`node ${ip}`, { env: process.env }).toString();
-    console.log(result);
+    // const ip = path.join(__dirname, 'index.js');
+    // const result = cp.execSync(`node ${ip}`, { env: process.env }).toString();
+    // console.log(result);
 })
